7e1ad56fc210e919ed4ed241fc503469e4f8ca74
A M Akhilesh <a.m.akhilesh336@gmail.com>
2022-07-05 23:13:02 +0530

    changed home image

--- crud_tut_fb/src/Displaycomponents/Home.js
+++ crud_tut_fb/src/Displaycomponents/Home.js
@@ -10,7 +10,7 @@ function Home() {
        Welcome Admin!!
     </div>
     <div className="flex justify-center p-10">
-<img className="" src="https://www.hindipanda.com/wp-content/uploads/2018/09/bharat-gas-booking.jpg"/>
+<img className="" alt="Online Gas Booking System" src="https://content.jdmagicbox.com/comp/tiruvallur/n2/9999pxx44.xx44.180630100847.b8n2/catalogue/the-vmc-gas-corporation-lpg-filling-plant-tiruvallur-lpg-gas-pipe-line-installation-services-mkddk5z5o8.jpg?clr="/>
 </div>
     </>
   );

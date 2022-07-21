66a8a840e1ac24744b00da78a7449fa350f4342d
A M Akhilesh <a.m.akhilesh336@gmail.com>
2022-07-22 01:47:32 +0530

    Updated Modal

--- crud_tut_fb/src/Displaycomponents/OrderDetails.js
+++ crud_tut_fb/src/Displaycomponents/OrderDetails.js
@@ -17,8 +17,11 @@ const userId=props.ordUid;
       const arrays=[];
       for (let id in data) {
         if(id==userId)
+        {
         arrays.push({ id, ...data[id] });
-      }
+        break;    
+        }  
+    }
       setUsers(arrays);
     //   if (data !== null) {
     //     Object.values(data).map((user) => {
@@ -101,18 +104,11 @@ const userId=props.ordUid;
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-white background-transparent bg-red-600 hover:bg-red-800 font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() =>{ setShowModal(false);}}
                   >
                     Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
                   </button>
                 </div>
               </div>
